a643991d6e97e42d153c7d4825e15904dfc09199
Yasemin-Toka <33073063+Yasemin-Toka@users.noreply.github.com>
2021-01-30 02:04:48 +0300

    Add project files.

---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MyDictionary");
+            Dictionary<int, string> MyDictionary = new Dictionary<int, string>();
+            MyDictionary.Add(1, "Armağan");
+            MyDictionary.Add(2, "Engin");
+            MyDictionary.Add(3, "Semiha");
+            // foreach list sample
+            Console.WriteLine("foreach sample for access MyDictionary Values");
+            foreach (string item in MyDictionary.Values)
+            {
+                Console.WriteLine(item);
+            }
+            // myDictionary.List sample
+            Console.WriteLine("MyDictionary.List");
+            MyDictionary.Last();
+            Console.ReadLine();
+        }
+    }
+
+}
+   
